fa0ef064194fa5b9b2c54413664b0e72773f5e07
eude313 <eudes.charles@student.moringaschool.com>
2021-08-25 17:45:55 +0300

    add initial modifications

--- app/request.py
+++ app/request.py
@@ -1,14 +1,10 @@
 import urllib.request,json
 
-
 # Getting api key
 secret_key = None
-# Getting the news base url
-quote_api = None
-
 
 def configure_request(app):
-    global  secret_key, quote_api
+    global  secret_key
     secret_key = app.config["SECRET_KEY"]
-    quote_api = app.config["QUOTES_API"]
+    
    
