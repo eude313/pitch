ad55dcfe47e925118c4cb4b2f232cb2a72e05807
eude313 <eudes.charles@student.moringaschool.com>
2021-08-25 17:46:36 +0300

    add initial modifications

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1234@localhost/blog'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
