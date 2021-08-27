fe88b281ff0e7bb2594325155c07bf6f9febc5ca
eude313 <eudes.charles@student.moringaschool.com>
2021-08-27 18:53:21 +0300

    add changes

--- webapp/models.py
+++ webapp/models.py
@@ -15,13 +15,12 @@ class User( db.Model,UserMixin ):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default= "before.jpg")
     password =db.Column(db.String(60), nullable=False)
     post = db.relationship('Post', backref='author', lazy=True)
     
     #how the object is printed when printed out
     def __repr__(self):
-        return f"User"("{self.username}", "{self.email}", "{self.image_file}")
+        return f"User"("{self.username}", "{self.email}")
     
 class Post( db.Model ):
     id = db.Column(db.Integer, primary_key=True)
