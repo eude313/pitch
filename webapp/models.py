76c5218781cb018301a6ff005009726058ddda27
eude313 <eudes.charles@student.moringaschool.com>
2021-08-27 09:22:24 +0300

    add all changes

--- webapp/models.py
+++ webapp/models.py
@@ -31,11 +31,11 @@ class Post (db.Model):
     '''
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    notes= db.Column(db.Text, nullable=False)
+    posts= db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Posts('{self.title}', '{self.date_posted}')"
+        return f"Posts('{self.title}')"
 
 
 
