8bde685ae8ce4edeaee1a124479afebab5d6a3e1
eude313 <eudes.charles@student.moringaschool.com>
2021-08-27 18:45:57 +0300

    add changes

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    QUOTES_API = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
