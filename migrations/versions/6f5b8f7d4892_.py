b2915547fbf9dba8417c557162e170acf294dc11
eude313 <eudes.charles@student.moringaschool.com>
2021-08-27 19:35:37 +0300

    add changes

---
+++ migrations/versions/6f5b8f7d4892_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6f5b8f7d4892
+Revises: 
+Create Date: 2021-08-27 19:33:59.269898
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6f5b8f7d4892'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
