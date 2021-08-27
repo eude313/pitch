0d84304de3b8cba776214470231f416a2d7861bd
eude313 <eudes.charles@student.moringaschool.com>
2021-08-27 15:15:40 +0300

    add all changes

--- webapp/auth/views.py
+++ webapp/auth/views.py
@@ -1,14 +1,14 @@
 from . import auth
 from webapp import db, bcrypt
 from flask_login import login_user, logout_user, current_user, login_required
-from ..models import User
-from flask import  render_template,url_for, flash, redirect
-from .forms import registrationForm, loginForm
+from ..models import User, Post
+from flask import render_template, url_for, flash, redirect
+from .forms import blog_form, registrationForm, loginForm
 
 @auth.route('/signup', methods=['POST', 'GET'])
 def signUp():
     if current_user.is_authenticated:
-        return redirect(url_for('main.home'))
+        return redirect(url_for('auth.signUp'))
     form = registrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -28,7 +28,7 @@ def signIn():
         user = User.query.filter_by(email= form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
            login_user(user, remember=form.remember.data)
-           return redirect(url_for('main.home')) 
+           return redirect(url_for('home')) 
         else:
             flash('login unsuccessful. please check your email or password.', 'danger')
     return render_template('signIn.html', form= form, title="signIn")
@@ -37,3 +37,21 @@ def signIn():
 def signOut():
     logout_user()
     return redirect(url_for('main.home')) 
+
+
+@auth.route('/blogs')
+def blogs():
+    posts= Post.query.all()
+    return render_template('blogs.html', posts=posts)
+
+@auth.route('/post/new', methods=['POST', 'GET'])
+@login_required
+def post():
+    form =blog_form()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash( " post has been created", "success" )
+        return redirect(url_for('auth.blogs'))
+    return render_template('post.html',form=form)
